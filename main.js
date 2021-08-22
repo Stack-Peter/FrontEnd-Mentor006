7dda45f1e1bab785cca6406b5ac6a823fab8704e
Pedro Marcos F <stack.peter21@gmail.com>
2021-08-22 15:15:27 -0300

    fixing css

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const btn = document.querySelector('button')
 const regexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
 
 
+
 function handleClick(e) {
     e.preventDefault()
     input.forEach((e) => {
